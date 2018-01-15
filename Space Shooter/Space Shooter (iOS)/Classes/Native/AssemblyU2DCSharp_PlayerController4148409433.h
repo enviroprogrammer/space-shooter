ab8dcb74b483befca8d8f5aeecd42ad059b25789
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2018-01-15 13:29:14 -0500

    initial commit

---
+++ Space Shooter/Space Shooter (iOS)/Classes/Native/AssemblyU2DCSharp_PlayerController4148409433.h
@@ -0,0 +1,139 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+
+#include "UnityEngine_UnityEngine_MonoBehaviour1158329972.h"
+
+// Boundary
+struct Boundary_t1794889402;
+// UnityEngine.Rigidbody
+struct Rigidbody_t4233889191;
+// UnityEngine.AudioSource
+struct AudioSource_t1135106623;
+// UnityEngine.GameObject
+struct GameObject_t1756533147;
+// UnityEngine.Transform
+struct Transform_t3275118058;
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// PlayerController
+struct  PlayerController_t4148409433  : public MonoBehaviour_t1158329972
+{
+public:
+	// System.Single PlayerController::speed
+	float ___speed_2;
+	// System.Single PlayerController::tilt
+	float ___tilt_3;
+	// Boundary PlayerController::boundary
+	Boundary_t1794889402 * ___boundary_4;
+	// UnityEngine.Rigidbody PlayerController::rb
+	Rigidbody_t4233889191 * ___rb_5;
+	// UnityEngine.AudioSource PlayerController::audioSource
+	AudioSource_t1135106623 * ___audioSource_6;
+	// UnityEngine.GameObject PlayerController::shot
+	GameObject_t1756533147 * ___shot_7;
+	// UnityEngine.Transform PlayerController::shotSpawn
+	Transform_t3275118058 * ___shotSpawn_8;
+	// System.Single PlayerController::fireRate
+	float ___fireRate_9;
+	// System.Single PlayerController::nextFire
+	float ___nextFire_10;
+
+public:
+	inline static int32_t get_offset_of_speed_2() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___speed_2)); }
+	inline float get_speed_2() const { return ___speed_2; }
+	inline float* get_address_of_speed_2() { return &___speed_2; }
+	inline void set_speed_2(float value)
+	{
+		___speed_2 = value;
+	}
+
+	inline static int32_t get_offset_of_tilt_3() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___tilt_3)); }
+	inline float get_tilt_3() const { return ___tilt_3; }
+	inline float* get_address_of_tilt_3() { return &___tilt_3; }
+	inline void set_tilt_3(float value)
+	{
+		___tilt_3 = value;
+	}
+
+	inline static int32_t get_offset_of_boundary_4() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___boundary_4)); }
+	inline Boundary_t1794889402 * get_boundary_4() const { return ___boundary_4; }
+	inline Boundary_t1794889402 ** get_address_of_boundary_4() { return &___boundary_4; }
+	inline void set_boundary_4(Boundary_t1794889402 * value)
+	{
+		___boundary_4 = value;
+		Il2CppCodeGenWriteBarrier(&___boundary_4, value);
+	}
+
+	inline static int32_t get_offset_of_rb_5() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___rb_5)); }
+	inline Rigidbody_t4233889191 * get_rb_5() const { return ___rb_5; }
+	inline Rigidbody_t4233889191 ** get_address_of_rb_5() { return &___rb_5; }
+	inline void set_rb_5(Rigidbody_t4233889191 * value)
+	{
+		___rb_5 = value;
+		Il2CppCodeGenWriteBarrier(&___rb_5, value);
+	}
+
+	inline static int32_t get_offset_of_audioSource_6() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___audioSource_6)); }
+	inline AudioSource_t1135106623 * get_audioSource_6() const { return ___audioSource_6; }
+	inline AudioSource_t1135106623 ** get_address_of_audioSource_6() { return &___audioSource_6; }
+	inline void set_audioSource_6(AudioSource_t1135106623 * value)
+	{
+		___audioSource_6 = value;
+		Il2CppCodeGenWriteBarrier(&___audioSource_6, value);
+	}
+
+	inline static int32_t get_offset_of_shot_7() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___shot_7)); }
+	inline GameObject_t1756533147 * get_shot_7() const { return ___shot_7; }
+	inline GameObject_t1756533147 ** get_address_of_shot_7() { return &___shot_7; }
+	inline void set_shot_7(GameObject_t1756533147 * value)
+	{
+		___shot_7 = value;
+		Il2CppCodeGenWriteBarrier(&___shot_7, value);
+	}
+
+	inline static int32_t get_offset_of_shotSpawn_8() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___shotSpawn_8)); }
+	inline Transform_t3275118058 * get_shotSpawn_8() const { return ___shotSpawn_8; }
+	inline Transform_t3275118058 ** get_address_of_shotSpawn_8() { return &___shotSpawn_8; }
+	inline void set_shotSpawn_8(Transform_t3275118058 * value)
+	{
+		___shotSpawn_8 = value;
+		Il2CppCodeGenWriteBarrier(&___shotSpawn_8, value);
+	}
+
+	inline static int32_t get_offset_of_fireRate_9() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___fireRate_9)); }
+	inline float get_fireRate_9() const { return ___fireRate_9; }
+	inline float* get_address_of_fireRate_9() { return &___fireRate_9; }
+	inline void set_fireRate_9(float value)
+	{
+		___fireRate_9 = value;
+	}
+
+	inline static int32_t get_offset_of_nextFire_10() { return static_cast<int32_t>(offsetof(PlayerController_t4148409433, ___nextFire_10)); }
+	inline float get_nextFire_10() const { return ___nextFire_10; }
+	inline float* get_address_of_nextFire_10() { return &___nextFire_10; }
+	inline void set_nextFire_10(float value)
+	{
+		___nextFire_10 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ Space Shooter/Space Shooter (iOS)/Classes/Native/Il2CppCompilerCalculateTypeValues_17Table.cpp
@@ -0,0 +1,254 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UI_U3CPrivateImplementationDetailsU3E_1568637717.h"
+#include "AssemblyU2DCSharp_U3CModuleU3E3783534214.h"
+#include "AssemblyU2DCSharp_DestroyByBoundary3535450619.h"
+#include "AssemblyU2DCSharp_DestroyByContact3397533383.h"
+#include "AssemblyU2DCSharp_DestroyByTime1319923286.h"
+#include "AssemblyU2DCSharp_GameController3607102586.h"
+#include "AssemblyU2DCSharp_GameController_U3CSpawnWavesU3Ec_495518909.h"
+#include "AssemblyU2DCSharp_Mover873752897.h"
+#include "AssemblyU2DCSharp_Boundary1794889402.h"
+#include "AssemblyU2DCSharp_PlayerController4148409433.h"
+#include "AssemblyU2DCSharp_RandomRotator1977862972.h"
+#include "AssemblyU2DCSharp_Readme795099402.h"
+#include "AssemblyU2DCSharp_Readme_Section1120322242.h"
+#include "AssemblyU2DCSharp_Done_BGScroller1955972008.h"
+#include "AssemblyU2DCSharp_Done_DestroyByBoundary1508105942.h"
+#include "AssemblyU2DCSharp_Done_DestroyByContact866731992.h"
+#include "AssemblyU2DCSharp_Done_DestroyByTime1767105437.h"
+#include "AssemblyU2DCSharp_Done_EvasiveManeuver2572524317.h"
+#include "AssemblyU2DCSharp_Done_EvasiveManeuver_U3CEvadeU3E1651935502.h"
+#include "AssemblyU2DCSharp_Done_GameController2490533223.h"
+#include "AssemblyU2DCSharp_Done_GameController_U3CSpawnWave3736144616.h"
+#include "AssemblyU2DCSharp_Done_Mover3207174306.h"
+#include "AssemblyU2DCSharp_Done_Boundary381809497.h"
+#include "AssemblyU2DCSharp_Done_PlayerController1238573128.h"
+#include "AssemblyU2DCSharp_Done_RandomRotator4185270575.h"
+#include "AssemblyU2DCSharp_Done_WeaponController1887556875.h"
+
+
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1700 = { sizeof (U24ArrayTypeU3D12_t1568637717)+ sizeof (Il2CppObject), sizeof(U24ArrayTypeU3D12_t1568637717 ), 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1701 = { sizeof (U3CModuleU3E_t3783534220), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1702 = { sizeof (DestroyByBoundary_t3535450619), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1703 = { sizeof (DestroyByContact_t3397533383), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1703[4] = 
+{
+	DestroyByContact_t3397533383::get_offset_of_explosion_2(),
+	DestroyByContact_t3397533383::get_offset_of_playerExplosion_3(),
+	DestroyByContact_t3397533383::get_offset_of_scoreValue_4(),
+	DestroyByContact_t3397533383::get_offset_of_gameController_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1704 = { sizeof (DestroyByTime_t1319923286), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1704[1] = 
+{
+	DestroyByTime_t1319923286::get_offset_of_lifetime_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1705 = { sizeof (GameController_t3607102586), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1705[12] = 
+{
+	GameController_t3607102586::get_offset_of_hazard_2(),
+	GameController_t3607102586::get_offset_of_spawnValues_3(),
+	GameController_t3607102586::get_offset_of_hazardCount_4(),
+	GameController_t3607102586::get_offset_of_spawnWait_5(),
+	GameController_t3607102586::get_offset_of_startWait_6(),
+	GameController_t3607102586::get_offset_of_waveWait_7(),
+	GameController_t3607102586::get_offset_of_scoreText_8(),
+	GameController_t3607102586::get_offset_of_restartText_9(),
+	GameController_t3607102586::get_offset_of_gameOverText_10(),
+	GameController_t3607102586::get_offset_of_gameOver_11(),
+	GameController_t3607102586::get_offset_of_restart_12(),
+	GameController_t3607102586::get_offset_of_score_13(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1706 = { sizeof (U3CSpawnWavesU3Ec__Iterator0_t495518909), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1706[4] = 
+{
+	U3CSpawnWavesU3Ec__Iterator0_t495518909::get_offset_of_U24this_0(),
+	U3CSpawnWavesU3Ec__Iterator0_t495518909::get_offset_of_U24current_1(),
+	U3CSpawnWavesU3Ec__Iterator0_t495518909::get_offset_of_U24disposing_2(),
+	U3CSpawnWavesU3Ec__Iterator0_t495518909::get_offset_of_U24PC_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1707 = { sizeof (Mover_t873752897), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1707[2] = 
+{
+	Mover_t873752897::get_offset_of_speed_2(),
+	Mover_t873752897::get_offset_of_rb_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1708 = { sizeof (Boundary_t1794889402), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1708[4] = 
+{
+	Boundary_t1794889402::get_offset_of_xMin_0(),
+	Boundary_t1794889402::get_offset_of_xMax_1(),
+	Boundary_t1794889402::get_offset_of_zMin_2(),
+	Boundary_t1794889402::get_offset_of_zMax_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1709 = { sizeof (PlayerController_t4148409433), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1709[9] = 
+{
+	PlayerController_t4148409433::get_offset_of_speed_2(),
+	PlayerController_t4148409433::get_offset_of_tilt_3(),
+	PlayerController_t4148409433::get_offset_of_boundary_4(),
+	PlayerController_t4148409433::get_offset_of_rb_5(),
+	PlayerController_t4148409433::get_offset_of_audioSource_6(),
+	PlayerController_t4148409433::get_offset_of_shot_7(),
+	PlayerController_t4148409433::get_offset_of_shotSpawn_8(),
+	PlayerController_t4148409433::get_offset_of_fireRate_9(),
+	PlayerController_t4148409433::get_offset_of_nextFire_10(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1710 = { sizeof (RandomRotator_t1977862972), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1710[2] = 
+{
+	RandomRotator_t1977862972::get_offset_of_tumble_2(),
+	RandomRotator_t1977862972::get_offset_of_rb_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1711 = { sizeof (Readme_t795099402), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1711[4] = 
+{
+	Readme_t795099402::get_offset_of_icon_2(),
+	Readme_t795099402::get_offset_of_title_3(),
+	Readme_t795099402::get_offset_of_sections_4(),
+	Readme_t795099402::get_offset_of_loadedLayout_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1712 = { sizeof (Section_t1120322242), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1712[4] = 
+{
+	Section_t1120322242::get_offset_of_heading_0(),
+	Section_t1120322242::get_offset_of_text_1(),
+	Section_t1120322242::get_offset_of_linkText_2(),
+	Section_t1120322242::get_offset_of_url_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1713 = { sizeof (Done_BGScroller_t1955972008), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1713[3] = 
+{
+	Done_BGScroller_t1955972008::get_offset_of_scrollSpeed_2(),
+	Done_BGScroller_t1955972008::get_offset_of_tileSizeZ_3(),
+	Done_BGScroller_t1955972008::get_offset_of_startPosition_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1714 = { sizeof (Done_DestroyByBoundary_t1508105942), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1715 = { sizeof (Done_DestroyByContact_t866731992), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1715[4] = 
+{
+	Done_DestroyByContact_t866731992::get_offset_of_explosion_2(),
+	Done_DestroyByContact_t866731992::get_offset_of_playerExplosion_3(),
+	Done_DestroyByContact_t866731992::get_offset_of_scoreValue_4(),
+	Done_DestroyByContact_t866731992::get_offset_of_gameController_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1716 = { sizeof (Done_DestroyByTime_t1767105437), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1716[1] = 
+{
+	Done_DestroyByTime_t1767105437::get_offset_of_lifetime_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1717 = { sizeof (Done_EvasiveManeuver_t2572524317), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1717[9] = 
+{
+	Done_EvasiveManeuver_t2572524317::get_offset_of_boundary_2(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_tilt_3(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_dodge_4(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_smoothing_5(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_startWait_6(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_maneuverTime_7(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_maneuverWait_8(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_currentSpeed_9(),
+	Done_EvasiveManeuver_t2572524317::get_offset_of_targetManeuver_10(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1718 = { sizeof (U3CEvadeU3Ec__Iterator0_t1651935502), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1718[4] = 
+{
+	U3CEvadeU3Ec__Iterator0_t1651935502::get_offset_of_U24this_0(),
+	U3CEvadeU3Ec__Iterator0_t1651935502::get_offset_of_U24current_1(),
+	U3CEvadeU3Ec__Iterator0_t1651935502::get_offset_of_U24disposing_2(),
+	U3CEvadeU3Ec__Iterator0_t1651935502::get_offset_of_U24PC_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1719 = { sizeof (Done_GameController_t2490533223), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1719[12] = 
+{
+	Done_GameController_t2490533223::get_offset_of_hazards_2(),
+	Done_GameController_t2490533223::get_offset_of_spawnValues_3(),
+	Done_GameController_t2490533223::get_offset_of_hazardCount_4(),
+	Done_GameController_t2490533223::get_offset_of_spawnWait_5(),
+	Done_GameController_t2490533223::get_offset_of_startWait_6(),
+	Done_GameController_t2490533223::get_offset_of_waveWait_7(),
+	Done_GameController_t2490533223::get_offset_of_scoreText_8(),
+	Done_GameController_t2490533223::get_offset_of_restartText_9(),
+	Done_GameController_t2490533223::get_offset_of_gameOverText_10(),
+	Done_GameController_t2490533223::get_offset_of_gameOver_11(),
+	Done_GameController_t2490533223::get_offset_of_restart_12(),
+	Done_GameController_t2490533223::get_offset_of_score_13(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1720 = { sizeof (U3CSpawnWavesU3Ec__Iterator0_t3736144616), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1720[8] = 
+{
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U3CiU3E__1_0(),
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U3ChazardU3E__2_1(),
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U3CspawnPositionU3E__2_2(),
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U3CspawnRotationU3E__2_3(),
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U24this_4(),
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U24current_5(),
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U24disposing_6(),
+	U3CSpawnWavesU3Ec__Iterator0_t3736144616::get_offset_of_U24PC_7(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1721 = { sizeof (Done_Mover_t3207174306), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1721[1] = 
+{
+	Done_Mover_t3207174306::get_offset_of_speed_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1722 = { sizeof (Done_Boundary_t381809497), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1722[4] = 
+{
+	Done_Boundary_t381809497::get_offset_of_xMin_0(),
+	Done_Boundary_t381809497::get_offset_of_xMax_1(),
+	Done_Boundary_t381809497::get_offset_of_zMin_2(),
+	Done_Boundary_t381809497::get_offset_of_zMax_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1723 = { sizeof (Done_PlayerController_t1238573128), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1723[7] = 
+{
+	Done_PlayerController_t1238573128::get_offset_of_speed_2(),
+	Done_PlayerController_t1238573128::get_offset_of_tilt_3(),
+	Done_PlayerController_t1238573128::get_offset_of_boundary_4(),
+	Done_PlayerController_t1238573128::get_offset_of_shot_5(),
+	Done_PlayerController_t1238573128::get_offset_of_shotSpawn_6(),
+	Done_PlayerController_t1238573128::get_offset_of_fireRate_7(),
+	Done_PlayerController_t1238573128::get_offset_of_nextFire_8(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1724 = { sizeof (Done_RandomRotator_t4185270575), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1724[1] = 
+{
+	Done_RandomRotator_t4185270575::get_offset_of_tumble_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1725 = { sizeof (Done_WeaponController_t1887556875), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1725[4] = 
+{
+	Done_WeaponController_t1887556875::get_offset_of_shot_2(),
+	Done_WeaponController_t1887556875::get_offset_of_shotSpawn_3(),
+	Done_WeaponController_t1887556875::get_offset_of_fireRate_4(),
+	Done_WeaponController_t1887556875::get_offset_of_delay_5(),
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
